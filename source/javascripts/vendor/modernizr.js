9f805d2f68564d411beb27b82c4b1b5b737c5b16
timvandevelde <timvandevelde@voormedia.com>
2018-07-09 14:20:57 +0200

    Removing all from master

--- source/javascripts/vendor/modernizr.js
+++
@@ -1,3 +0,0 @@
-/* Generate a custom build according to your needs, starting with the below URL
- * http://modernizr.com/download/#-svg-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
- */
